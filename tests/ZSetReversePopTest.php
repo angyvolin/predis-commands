79ce83a1759db0de03fda2b252d6d45f35ec1b0c
Andrii Volin <angy.volin@gmail.com>
2017-05-01 15:34:58 +0300

    add test cases for zrpop command

--- tests/ZSetReversePopTest.php
+++ tests/ZSetReversePopTest.php
@@ -2,6 +2,7 @@
 
 namespace Angyvolin\Predis\Command\Tests;
 
+use Angyvolin\Predis\Command\ZSetReversePop;
 use Predis\Command\PredisCommandTestCase;
 
 /**
@@ -15,7 +16,7 @@ class ZSetReversePopTest extends PredisCommandTestCase
      */
     protected function getExpectedCommand()
     {
-        return 'Angyvolin\Predis\Command\ZSetReversePop';
+        return ZSetReversePop::class;
     }
 
     /**
@@ -26,9 +27,6 @@ class ZSetReversePopTest extends PredisCommandTestCase
         return 'EVALSHA';
     }
 
-    /**
-     * @group disconnected
-     */
     public function testFilterArguments()
     {
         $command = $this->getCommand();
@@ -37,11 +35,52 @@ class ZSetReversePopTest extends PredisCommandTestCase
         $this->assertSame(['77d83487b1ff942c692a299a6b0365dfb09ae500', 1, 'key'], $command->getArguments());
     }
 
-    /**
-     * @group disconnected
-     */
     public function testParseResponse()
     {
         $this->assertSame(1, $this->getCommand()->parseResponse(1));
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testPopsTheLastElementFromZSet()
+    {
+        $redis = $this->getClient();
+
+        $redis->zadd('letters', 1, 'a', 2.5, 'b');
+        $this->assertSame(['a', '1'], $redis->zrpop('letters'));
+        $this->assertSame(['b', '2.5'], $redis->zrpop('letters'));
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testReturnsEmptyArrayOnEmptyZSet()
+    {
+        $this->assertSame([], $this->getClient()->zrpop('letters'));
+    }
+
+    /**
+     * @group disconnected
+     * @expectedException \Predis\Response\ServerException
+     * @expectedExceptionMessage Operation against a key holding the wrong kind of value
+     */
+    public function testThrowsExceptionOnWrongType()
+    {
+        $redis = $this->getClient();
+
+        $redis->set('foo', 'bar');
+        $redis->zrpop('foo');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClient($flushDb = true)
+    {
+        $redis = parent::getClient($flushDb);
+        $redis->getProfile()->defineCommand('zrpop', ZSetReversePop::class);
+
+        return $redis;
     }
 }
